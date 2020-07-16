d5912d77cae6eab0c1956a6ab39d47ffa426da42
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-07-16 12:25:15 -0300

    feat(types): add type for express Middlewares

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,6 @@
+import { Request, Response, NextFunction } from 'express'
+import { ResponseOrPromise } from '../controllers/Controller'
+
 /**
  * This file exports all types that we use in more than one place,
  * this makes the types always the same, avoiding errors
@@ -10,3 +13,10 @@
   */
 export type Env = 'prod' // I use prod instead of production, but you can change
  | 'dev' | 'test'
+
+/**
+ * Express middleware type
+ */
+
+export type Middleware = (req: Request, res: Response, next: NextFunction) =>
+  ResponseOrPromise | NextFunction
